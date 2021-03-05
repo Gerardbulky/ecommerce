35b893dd0d493bad119026a86dc12f6a79fff26a
gerardambe <gerardambe@yahoo.com>
2021-03-05 00:05:16 +0000

    added checkout success logic

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'w74r#w^zid-+=7pq_1f=e@8)z@5wuyb2m_f4hw_r&+$75n*m0='
+SECRET_KEY = 'SECRET_KEY'
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
