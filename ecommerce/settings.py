32d8b64f342ee1547bede42bf42d76dc44b503e4
gerardambe <gerardambe@yahoo.com>
2021-05-26 11:16:15 +0000

    061 sending real emails

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -108,9 +108,6 @@ AUTHENTICATION_BACKENDS = [
 
 SITE_ID = 1         # 4thly site_id
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'  # Emails confirmat settx
-
-
 ACCOUNT_AUTHENTICATION_METHOD = 'username_email' # allows auth using usernam/email
 ACCOUNT_EMAIL_REQUIRED = True                # email required to register 4 the site
 ACCOUNT_EMAIL_VERIFICATION = 'mandatory'    # verifying if users are using real email
@@ -212,4 +209,15 @@ STRIPE_CURRENCY = 'usd'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
-DEFAULT_FROM_EMAIL = 'bossmanboutique@example.com'
+
+if 'DEVELOPMENT' in os.environ:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    DEFAULT_FROM_EMAIL = 'bossmanboutique@example.com'
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
